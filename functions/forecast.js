a23ee1d0199064bb8cb4e97a35c81da3cfac722b
mark l chaves <mark@marklchaves.com>
2019-12-09 02:43:50 +0800

    Create forecast.js

---
+++ functions/forecast.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  try{
+    const latitude  = event.queryStringParameters.latitude;
+    const longitude = event.queryStringParameters.longitude;
+
+    const response = await fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${latitude},${longitude}`);
+    const data     = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString()
+    };
+  }
+};
