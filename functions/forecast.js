76ec14b92d34dc188b7e06cea44c432f06da30f2
mark l chaves <mark@marklchaves.com>
2019-12-09 20:22:59 +0800

    Update forecast.js

--- functions/forecast.js
+++ functions/forecast.js
@@ -5,14 +5,16 @@ exports.handler = async (event, context) => {
   try{
     const latitude  = event.queryStringParameters.latitude;
     const longitude = event.queryStringParameters.longitude;
+    
+    return await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`);
 
-    const response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`);
+/*    const response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`);
     const data     = await response.json();
 
     return {
       statusCode: 200,
       body: JSON.stringify(data)
-    };
+    }; */
   } catch (err) {
     return {
       statusCode: 500,
